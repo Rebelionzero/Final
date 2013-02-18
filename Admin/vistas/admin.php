bc3e8e01434522f04683bef337a44fdad90fc2e7
Rebelionzero <toda_creacion@hotmail.com>
2013-02-18 20:03:02 -0200

    modified admin.php

--- Admin/vistas/admin.php
+++ Admin/vistas/admin.php
@@ -1,6 +1,6 @@
 <?php
 	session_start();
-	
+
 	include_once("../autoloader.php");
 
 	$conexion = new Conexion();
@@ -30,7 +30,7 @@
 		}
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html>
 	<head>
 		<title>Shop Smart Admin Panel</title>
